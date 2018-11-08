333814d9e78278a1219310aaba69cf8562ddd4aa
sgtpepper9091 <c.stacey9091@gmail.com>
2018-11-08 17:45:50 -0330

    Update Physics.cpp

--- src/Physics.cpp
+++ src/Physics.cpp
@@ -3,7 +3,12 @@
 
 Vec2 Physics::GetOverlap(std::shared_ptr<Entity> a, std::shared_ptr<Entity> b)
 {
-    return Vec2(0, 0);
+	Vec2 delta(abs((a->getComponent<CTransform>()->pos.x) - (b->getComponent<CTransform>()->pos.x)), (abs((a->getComponent<CTransform>()->pos.y) - (b->getComponent<CTransform>()->pos.y))));
+
+	float ox = (a->getComponent<CBoundingBox>()->halfSize.x) + (b->getComponent<CBoundingBox>()->halfSize.x) - delta.x;
+	float oy = (a->getComponent<CBoundingBox>()->halfSize.y) + (b->getComponent<CBoundingBox>()->halfSize.y) - delta.y;
+
+	return Vec2(ox, oy);
 }
 
 Vec2 Physics::GetPreviousOverlap(std::shared_ptr<Entity> a, std::shared_ptr<Entity> b)
