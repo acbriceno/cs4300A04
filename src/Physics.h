ead88fb54b04c0efe9c117f2d55488e05e4e7d82
sgtpepper9091 <c.stacey9091@gmail.com>
2018-11-15 23:46:39 -0330

    Update Physics.h

--- src/Physics.h
+++ src/Physics.h
@@ -11,5 +11,5 @@ namespace Physics
     Vec2 GetPreviousOverlap(std::shared_ptr<Entity> a, std::shared_ptr<Entity> b);
     Intersect LineIntersect(const Vec2 & a, const Vec2 & b, const Vec2 & c, const Vec2 & d);
     bool EntityIntersect(const Vec2 & a, const Vec2 & b, std::shared_ptr<Entity> e);
-    Vec2 GetOverlap(std::shared_ptr<Entity> a, std::shared_ptr<Entity> b, Vec2 pos);
+    
 }
